b4d8d082e4d127d5191fcc9b30f0d26c2b59b7ca
Jyri Lindroos <jyri.lindroos@keuda.fi>
2024-12-18 08:05:17 +0200

    Chapter 7 loppuun asti

--- Chapter07/main.js
+++ Chapter07/main.js
@@ -9,9 +9,41 @@ $(document).ready(() => {
 
   const panelHeight = $("#chocolate").outerHeight();
   console.log("Panel height: " + panelHeight);
+  
+  const panelTop = $("#chocolate").offset().top;
+  console.log("Top of the panel, position: " + panelTop);
 
-  const hideAmount = panelHeight - 50;
+  const secondParaTop = $("#chocolate p:nth-child(2)").offset().top;
+  console.log("Top of 2nd P, position: " + secondParaTop);
+
+  const showAmount = parseInt(secondParaTop - panelTop);
+  console.log("Height of first P in panel: " + showAmount);
+
+  const hideAmount = panelHeight - showAmount;
+  console.log("Height of panel to be hidden: " + hideAmount);
+
+
 
   $("#chocolate").css("bottom", -hideAmount);
   console.log("Hidden height of panel: " + hideAmount);
+
+  $("#chocolate").on('click', () =>{
+    if($("#chocolate").hasClass('up')){
+      // up -> down
+      $("#chocolate").animate({
+        bottom: -hideAmount
+      }, 300).removeClass('up');
+
+    }
+    else{
+      // down -> up
+      $("#chocolate").animate({
+        bottom: "0"
+      }, 300).addClass('up');
+    }
+
+
+  });
+
+
 });
